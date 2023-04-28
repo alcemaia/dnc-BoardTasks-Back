55b1d9614c27af3ccc5dbb7ebaff79a948a1511c
AlceMaia <alceniziamaia@gmail.com>
2023-04-28 01:16:29 -0300

    criação do esquema mongoose tarefa

--- src/models/tarefa.js
+++ src/models/tarefa.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 
 const esquema = new mongoose.Schema(
   {
-    posicao: {
-      type: Number,
+    posicao: { /*antes do posição tem o id mas é criado automaticamente pelo mongoose*/
+      type: Number, /*ser a posição que a tarefa está*/
       required: 'é obrigatório!',
     },
     titulo: {
       type: String,
       required: 'é obrigatório!',
     },
-    descricao: {
+    descricao: { /*como não é obrigatorio o default preenche com uma string vazia. se não colocar o mongoose não vai definir o campo na estrutura da tarefa no front */
       type: String,
       default: '',
     },
-    status: {
+    status: { /*qual estatos a tarefa está */
       type: String,
       required: 'é obrigatório!',
     },
@@ -24,7 +24,7 @@ const esquema = new mongoose.Schema(
     },
     usuarioCriador: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Usuario',
+      ref: 'Usuario', /*ele está referenciado com a collectio usuário - saber de quem é a tarefa */
       required: 'é obrigatório!',
     },
   },
